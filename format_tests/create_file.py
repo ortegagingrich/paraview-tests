22774ba80f1c05f27f740690ae2f4529b5d102f1
JOG-HLD <ortegagingrich@gmail.com>
2015-11-18 06:50:23 -0800

    Topography is working?

--- format_tests/create_file.py
+++ format_tests/create_file.py
@@ -1,11 +1,12 @@
 """ Create a simple vtk file from a numpy array """
 import numpy as np
 
-TESTING = False
+TESTING = True
 
 def export_to_vtk(xgrid, ygrid, data, data_name):
 	""" Export the specified 2D structured grid to file """
 	from evtk.vtk import VtkFile, VtkStructuredGrid
+	
 	
 	#stupid reshape data
 	oldshape = data.shape
@@ -13,6 +14,7 @@ def export_to_vtk(xgrid, ygrid, data, data_name):
 	data = data.reshape(newshape)
 	xgrid = xgrid.reshape(newshape)
 	ygrid = ygrid.reshape(newshape)
+	
 	
 	path = './{}'.format(data_name)
 	w = VtkFile(path, VtkStructuredGrid)
@@ -48,7 +50,7 @@ def __create_sample_data__(npts = 20):
 		return np.sin(0.2*np.pi*x)*np.cos(0.4*np.pi*y)
 	
 	#make grid
-	xs = np.linspace(0, 20, npts + 1)
+	xs = np.linspace(0, 2*20, 2*npts + 1)
 	ys = np.linspace(0, 20, npts + 1)
 	(xgrid, ygrid) = np.meshgrid(xs, ys)
 	zgrid = wavy(xgrid, ygrid)
@@ -56,18 +58,70 @@ def __create_sample_data__(npts = 20):
 	return (xgrid, ygrid, zgrid)
 
 
+def __load_topography__(filepath):
+	""" Load in topography to numpy arrays (to send to paraview as vtk) """
+	from clawpack.geoclaw import topotools
+	topo = topotools.Topography(filepath)
+	
+	if TESTING:
+		import matplotlib.pyplot as plt
+		topo.plot()
+		plt.show()
+	topo.topo_type = 3
+	xgrid = topo.X
+	ygrid = topo.Y
+	zgrid = topo.Z
+	
+	#temp; find a better solution (e.g. convert from lat/lon to actual space)
+	#xgrid = 1.e4 * xgrid
+	#ygrid = 1.e4 * ygrid
+	
+	#test only
+	shape = zgrid.shape
+	ny, nx = shape[0], shape[1]
+	#for iy in range(0,ny):
+		#zgrid[iy, 0] = zgrid[iy,0]+1e4
+	#for ix in range(0,nx):
+		#zgrid[1, ix] = zgrid[1,ix]-1e4
+	
+	def wavy(x, y):
+		return np.sin(0.2*np.pi*x)*np.cos(0.4*np.pi*y)
+	
+	wavyz = wavy(xgrid, ygrid)
+	
+	
+	for ix in range(0,250):
+		for iy in range(0,400):
+			zgrid[iy, ix] = 1e4*wavyz[iy, ix]
+	
+	zgrid = 1e-4 * zgrid
+	
+	return (xgrid, ygrid, zgrid)
+		
+	
+
+
 def __test_main__():
 	import matplotlib.pyplot as plt
+	import os
 	
-	(xgrid, ygrid, zgrid) = __create_sample_data__(npts = 1000)
+	topo_dir = os.environ['TOPO']
+	topo_file = os.path.join(topo_dir, 'etopo1-131122039053.tt3')
+	(xgrid, ygrid, zgrid) = __load_topography__(topo_file)
+	
+	
+	#(xgrid, ygrid, zgrid) = __create_sample_data__(npts = 1000)
 	
 	#just for testing
 	if TESTING:
+		print xgrid
+		print ygrid
+		print zgrid.shape
 		plt.contourf(xgrid, ygrid, zgrid)
 		plt.show()
 	
 	#export to file
-	export_to_vtk(xgrid, ygrid, zgrid, 'topo')
+	export_to_vtk(xgrid, ygrid, zgrid, 'topo_grays')
 
 
 if __name__ == '__main__':
